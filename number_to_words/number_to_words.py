92e0d46f50a345991c25dcf9732e672b1e9f39e2
Jesus Vedasto A. Olazo <jestoy.olazo@gmail.com>
2024-07-07 21:02:51 +0400

    Update number_to_words.py
    Convert the codes to be able to run in Python version 3.

--- number_to_words/number_to_words.py
+++ number_to_words/number_to_words.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # number_to_words.py - A simple number to words converter.
-# Copyright (c) 2016 - Jesus Vedasto Olazo - jessie@jestoy.frihost.net
+# Copyright (c) 2016 - Jesus Vedasto Olazo - jestoy.olazo@gmail.com
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -48,18 +48,18 @@ def step_one(number):
 
 def step_two(number):
     if number % 10 == 0:
-        result = second_list[(number /10)-2]
-    elif (number / 10) > 1 and (number / 10) <= 9:
-        first_part = second_list[(number / 10)-2]
+        result = second_list[(number // 10) - 2]
+    elif (number // 10) > 1 and (number // 10) <= 9:
+        first_part = second_list[(number // 10) - 2]
         second_part = first_list[number % 10]
         result = " ".join([first_part, second_part])
     return result
 
 def step_three(number):
     if number % 100 == 0:
-        result =  first_list[number/100] + " hundred"
+        result = first_list[number // 100] + " hundred"
     elif number % 100 > 0 and number % 100 <= 99:
-        part1 = first_list[number/100] + " hundred "
+        part1 = first_list[number // 100] + " hundred "
         if (number % 100) >= 0 and (number % 100) <= 19:
             part2 = step_one(number % 100)
         elif (number % 100) >= 20 and (number % 100) <= 99:
@@ -69,20 +69,20 @@ def step_three(number):
 
 def step_four(number):
     if number % 1000 == 0:
-        if (number / 1000) > 0 and (number / 1000) <= 19:
-            result = step_one(number / 1000) + " thousand"
-        elif (number / 1000) >= 20 and (number / 1000) <= 99:
-            result = step_two(number / 1000) + " thousand"
-        elif number / 1000 >= 100 and number / 1000 <= 999:
-            result = step_three(number / 1000) + " thousand"
+        if (number // 1000) > 0 and (number // 1000) <= 19:
+            result = step_one(number // 1000) + " thousand"
+        elif (number // 1000) >= 20 and (number // 1000) <= 99:
+            result = step_two(number // 1000) + " thousand"
+        elif number // 1000 >= 100 and number // 1000 <= 999:
+            result = step_three(number // 1000) + " thousand"
 
     elif number % 1000 > 0 and number % 1000 <= 999:
-        if number / 1000 > 0 and number / 1000 <= 19:
-            part1 = step_one(number / 1000) + " thousand "
-        elif number / 1000 >= 20 and number / 1000 <= 99:
-            part1 = step_two(number / 1000) + " thousand "
-        elif number / 1000 >= 100 and number / 1000 <= 999:
-            part1 = step_three(number / 1000) + " thousand "
+        if number // 1000 > 0 and number // 1000 <= 19:
+            part1 = step_one(number // 1000) + " thousand "
+        elif number // 1000 >= 20 and number // 1000 <= 99:
+            part1 = step_two(number // 1000) + " thousand "
+        elif number // 1000 >= 100 and number // 1000 <= 999:
+            part1 = step_three(number // 1000) + " thousand "
 
         if number % 1000 > 0 and number % 1000 < 20:
             part2 = step_one(number % 1000)
@@ -95,20 +95,20 @@ def step_four(number):
 
 def step_five(number):
     if number % 1000000 == 0:
-        if (number / 1000000) > 0 and (number / 1000000) < 20:
-            result = " ".join([step_one(number / 1000000), "million"])
-        elif (number / 1000000) > 19 and (number / 1000000) < 100:
-            result = " ".join([step_two(number / 1000000), "million"])
-        elif (number / 1000000) > 99 and (number / 1000000) < 1000:
-            result = " ".join([step_three(number / 1000000), "million"])
+        if (number // 1000000) > 0 and (number // 1000000) < 20:
+            result = " ".join([step_one(number // 1000000), "million"])
+        elif (number // 1000000) > 19 and (number // 1000000) < 100:
+            result = " ".join([step_two(number // 1000000), "million"])
+        elif (number // 1000000) > 99 and (number // 1000000) < 1000:
+            result = " ".join([step_three(number // 1000000), "million"])
 
     elif number % 1000000 > 0 and number % 1000000 < 1000000000:
-        if number / 1000000 > 0 and number / 1000000 < 20:
-            first_part = step_one(number / 1000000) + " million "
-        elif number / 1000000 > 19 and number / 1000000 < 100:
-            first_part = step_two(number / 1000000) + " million "
-        elif number / 1000000 > 99 and number / 1000000 < 1000:
-            first_part = step_three(number / 1000000) + " million "
+        if number // 1000000 > 0 and number // 1000000 < 20:
+            first_part = step_one(number // 1000000) + " million "
+        elif number // 1000000 > 19 and number // 1000000 < 100:
+            first_part = step_two(number // 1000000) + " million "
+        elif number // 1000000 > 99 and number // 1000000 < 1000:
+            first_part = step_three(number // 1000000) + " million "
 
         if number % 1000000 > 0 and number % 1000000 < 20:
             second_part = step_one(number % 1000000)
@@ -124,7 +124,7 @@ def step_five(number):
 
 def number_to_words(number):
     """
-    This the main function that should be called when converting number
+    This is the main function that should be called when converting number
     into words. This function will decide which of other function should be
     called on with regards to the given number. Below is an example.
 
@@ -180,29 +180,29 @@ def number_to_words(number):
         return None
 
 if __name__ == "__main__":
-    print "A simple number to words converter."
-    print "Range: 0 to 999,999,999\n\n"
+    print("A simple number to words converter.")
+    print("Range: 0 to 999,999,999\n\n")
     # Example 1
-    print "Example 1:"
-    print "%s = %s" % (str(2), number_to_words(2))
+    print("Example 1:")
+    print("%s = %s" % (str(2), number_to_words(2)))
     # Example 2
-    print "Example 2:"
-    print "%s = %s" % (str(34), number_to_words(34))
+    print("Example 2:")
+    print("%s = %s" % (str(34), number_to_words(34)))
     # Example 3
-    print "Example 3:"
-    print "%s = %s" % (str(121), number_to_words(121))
+    print("Example 3:")
+    print("%s = %s" % (str(121), number_to_words(121)))
     # Example 4
-    print "Example 4:"
-    print "%s = %s" % (str(2378), number_to_words(2378))
+    print("Example 4:")
+    print("%s = %s" % (str(2378), number_to_words(2378)))
     # Example 5
-    print "Example 5:"
-    print "%s = %s" % (str(13725.4534), number_to_words(13725.4534))
+    print("Example 5:")
+    print("%s = %s" % (str(13725.4534), number_to_words(13725.4534)))
     # Example 6
-    print "Example 6:"
-    print "%s = %s" % (str(245312), number_to_words(245312))
+    print("Example 6:")
+    print("%s = %s" % (str(245312), number_to_words(245312)))
     # Example 7
-    print "Example 7:"
-    print "%s = %s" % (str(1000000), number_to_words(1000000))
+    print("Example 7:")
+    print("%s = %s" % (str(1000000), number_to_words(1000000)))
     # Example 8
-    print "Example 8:"
-    print "%s = %s" % (str(6782215), number_to_words(6782215))
+    print("Example 8:")
+    print("%s = %s" % (str(6782215), number_to_words(6782215)))
